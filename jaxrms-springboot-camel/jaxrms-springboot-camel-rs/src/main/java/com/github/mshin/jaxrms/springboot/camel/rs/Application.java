7fb987578859fce95fa4e7d790693496afce5b32
MunChul Shin <mshin@redhat.com>
2021-01-06 12:01:06 -0800

    ading exception response handlers.

--- jaxrms-springboot-camel/jaxrms-springboot-camel-rs/src/main/java/com/github/mshin/jaxrms/springboot/camel/rs/Application.java
+++ jaxrms-springboot-camel/jaxrms-springboot-camel-rs/src/main/java/com/github/mshin/jaxrms/springboot/camel/rs/Application.java
@@ -3,7 +3,6 @@ package com.github.mshin.jaxrms.springboot.camel.rs;
 import java.util.Arrays;
 
 import org.apache.cxf.Bus;
-import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.github.mshin.exception.response.handler.ExceptionResponseHandler;
+import com.github.mshin.exception.response.handler.ExceptionResponsesHandler;
 import com.github.mshin.jaxrms.rs.api.JaxrmsService;
 
 @SpringBootApplication
@@ -30,11 +31,10 @@ public class Application {
         JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();
         factoryBean.setBus(bus);
         factoryBean.setAddress("/");
-        factoryBean.setProviders(Arrays.asList(new JacksonJsonProvider()));
+        factoryBean.setProviders(Arrays.asList(new ExceptionResponseHandler(), new ExceptionResponsesHandler(),
+                new JacksonJsonProvider()));
         factoryBean.setFeatures(Arrays.asList(swagger2Feature()));
-
         factoryBean.setServiceClass(JaxrmsService.class);
-        // factoryBean.create();
         return factoryBean;
     }
 
